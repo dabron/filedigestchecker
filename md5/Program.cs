9a13ec035a74699db48b8f163abf1e34262599e3
David Bronleewe <david@bronleewe.net>
2011-02-14 09:47:17 -0600

    initial commit

---
+++ md5/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace md5
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ sha1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace sha1
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+		}
+	}
+}
